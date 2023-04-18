87533957d8cecb8a8eada1d8c249c6a65b2b690b
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-04-18 20:31:00 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:responsividade_mediaquery/responsividade_media_query.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Flutter Web",
+    debugShowCheckedModeBanner: false,
+    home: ResponsividadeMediaQuery(),
+  ));
+}
---
+++ lib/responsividade_media_query.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ResponsividadeMediaQuery extends StatefulWidget {
+  const ResponsividadeMediaQuery({super.key});
+
+  @override
+  State<ResponsividadeMediaQuery> createState() =>
+      _ResponsividadeMediaQueryState();
+}
+
+class _ResponsividadeMediaQueryState extends State<ResponsividadeMediaQuery> {
+  @override
+  Widget build(BuildContext context) {
+    var largura = MediaQuery.of(context).size.width;
+    var altura = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Responsividade'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: largura,
+            height: altura,
+            color: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
+}
